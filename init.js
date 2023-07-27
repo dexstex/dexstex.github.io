15642d8af9613e02b9f7e19504a9dbf30c2a7139
dexstex <lohdexstex22@gmail.com>
2023-07-27 11:28:00 +0800

    added hidden messages for presence

--- init.js
+++ init.js
@@ -17,6 +17,16 @@ async function init(data) {
         jsonact.classList.add("dnd")
     } else {
         jsonact.classList.add("offline");
+
+        let currentdiv = document.getElementById("activities");
+        var div = document.createElement("div");
+        div.id = "offline";
+        div.className = "activity";
+        currentdiv.appendChild(div);
+
+        let textarray = ["Doing nothing", "Probably in school", "Hi :3", "Dead X_X", "PC exploded", "Probably touching grass"]
+        var rand = textarray[Math.random() * textarray.length | 0]
+        div.innerHTML = (rand)
     }
     jsonact.src = "https://cdn.discordapp.com/avatars/" + "545719664983015434" + "/" + json.discord_user['avatar'];
     document.getElementById("name").innerHTML = json.discord_user['username'];
@@ -25,9 +35,6 @@ async function init(data) {
     var h = originalh;
     let curractiv = 1;
     activities.forEach(element => {
-        // if not the status activity, continue
-        // this is where the code and spotify div are made
-
         if(element['type'] !== 4) {
             var div = document.createElement("div");
             div.id = element['name'].split(' ').join('').toLowerCase();
@@ -179,28 +186,31 @@ async function updatepresence() {
             }
             h += addedh;
 
-
             if(exists)
                 currentdiv.appendChild(div);
         }
     });
 
-    // get the difference of the current activities and the last, mostly just to remove activities that aren't active anymore
-    let names = [];
-    activities.forEach(e => {if(e['type'] !== 4)names.push(e['name'].split(' ').join('').toLowerCase())})
-    var children = [].slice.call(currentdiv.getElementsByClassName('activity'), 0);
-    var childnames = new Array(children.length);
-    var array1Length = children.length;
-    var array2Length = names.length;
-    for (var i = 0; i < array1Length; i++) {
-        var name = children[i].getAttribute("id");    
-        childnames[i] = name;
-    }
-    var toremove = childnames.filter(x => !names.includes(x));
-    children.filter(x => toremove.includes(x.id)).forEach(e => {e.remove()});
+    if (json['discord_status'] == "offline") {
 
-    // set height of activities rect
-    currentdiv.style.height = h + "rem";
+    } else {
+        // get the difference of the current activities and the last, mostly just to remove activities that aren't active anymore
+        let names = [];
+        activities.forEach(e => {if(e['type'] !== 4)names.push(e['name'].split(' ').join('').toLowerCase())})
+        var children = [].slice.call(currentdiv.getElementsByClassName('activity'), 0);
+        var childnames = new Array(children.length);
+        var array1Length = children.length;
+        var array2Length = names.length;
+        for (var i = 0; i < array1Length; i++) {
+            var name = children[i].getAttribute("id");    
+            childnames[i] = name;
+        }
+        var toremove = childnames.filter(x => !names.includes(x));
+        children.filter(x => toremove.includes(x.id)).forEach(e => {e.remove()});
+    
+        // set height of activities rect
+        currentdiv.style.height = h + "rem";
+    }
 }
 
 const onload = async () => {
