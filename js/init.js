56ba6aa1cb2816bb148838f449b349273d76194a
lohdexstex22@gmail.com <lohdexstex22@gmail.com>
2023-08-31 18:45:15 +0800

    optimise js slightly

--- js/init.js
+++ js/init.js
@@ -2,14 +2,15 @@ var userid = "545719664983015434";
 async function init(i) {
   let e = i,
     t = document.getElementById("istatusdot");
-  c = document.getElementById("istatustext");
-  if ("online" === e.discord_status) {
+    c = document.getElementById("istatustext");
+    d = e.discord_status;
+  if ("online" === d) {
     t.classList.add("online");
     c.innerHTML = "Currently Online";
-  } else if ("idle" === e.discord_status) {
+  } else if ("idle" === d) {
     t.classList.add("idle");
     c.innerHTML = "Currently Idle";
-  } else if ("dnd" === e.discord_status) {
+  } else if ("dnd" === d) {
     t.classList.add("dnd");
     c.innerHTML = "Currently on Do Not Disturb";
   } else {
